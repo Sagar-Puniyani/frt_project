d4ca15ec5832517d0c0ad310fef00986335d1a93
Sagar <SagarPuniyani@gmail.com>
2023-06-29 11:54:10 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { ChatEngine } from "react-chat-engine"; 
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="f1db7b3b-8a49-4f80-baa9-355fa6663d4f"
+            userName="Sagar Puniyani"
+            userSecret="Sagar@1207"
+        />
+    );
+}
+
+export default App;
